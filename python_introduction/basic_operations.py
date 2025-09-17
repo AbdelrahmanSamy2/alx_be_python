ff686d6ce949c7b5c0e4ba5a4e2ef7a397a47508
Abdelrahman SAMY <abdelrahman.samy@valeo.com>
2025-09-18 01:44:18 +0300

    updating basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -12,10 +12,10 @@
 number1 = 10
 number2 = 5
 addition = number1 + number2
-subtraction = number2 - number1
-multiplication = number1 * number2  
+subtraction = number1 - number2
+multiplication = number1 * number2
 
 
-print("Addition of 10 and 5 is 15")
-print("Subtraction of 10 and 5 is 5")
-print("Multiplication of 10 and 5 is 50")
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
